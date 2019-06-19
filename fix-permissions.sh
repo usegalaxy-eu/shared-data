14c4798bdf6eaa79e17ddd9fcc6049254c8cbdd9
Helena Rasche <hxr@hx42.org>
2019-06-19 16:49:28 +0200

    initial version

---
+++ fix-permissions.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+for dataset in $(parsec libraries show_library 7d84c2ac21245dad --contents | jq '.[] | select(.type == "file") | .id' -r); do
+	echo -n "$dataset "
+	for _ in $(seq 1 10); do
+		echo -n '.'
+		output="$(curl --silent "https://usegalaxy.eu/api/libraries/datasets/$dataset/permissions?action=set_permissions&key=$GALAXY_API_KEY" --data '')"
+		echo "$output" | grep --quiet access_dataset_roles
+		ec=$?
+		if (( ec == 0 )); then
+			echo ''
+			break
+		else
+			sleep 1
+		fi
+	done;
+done;
---
+++ run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+setup-data-libraries -g https://usegalaxy.eu -a $GALAXY_API_KEY -vvv --legacy -i GTN.yaml 2>&1 | grep -v DEBUG
+
+for dataset in $(parsec libraries show_library 7d84c2ac21245dad --contents | jq '.[] | select(.type == "file") | .id' -r); do
+	echo -n "$dataset "
+	for _ in $(seq 1 10); do
+		echo -n '.'
+		output="$(curl --silent "https://usegalaxy.eu/api/libraries/datasets/$dataset/permissions?action=set_permissions&key=$GALAXY_API_KEY" --data '')"
+		echo "$output" | grep --quiet access_dataset_roles
+		ec=$?
+		if (( ec == 0 )); then
+			echo ''
+			break
+		else
+			sleep 1
+		fi
+	done;
+done;
