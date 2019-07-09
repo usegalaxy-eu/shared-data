a3d8704ad0b9b4a2188d785cce558608204cfb33
Helena Rasche <hxr@hx42.org>
2019-07-09 13:33:05 +0200

    remove parsec

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ setup_library() {
 	setup-data-libraries -g $server -a $key -vvv --legacy -i GTN.yaml 2>&1 | grep --line-buffered -v DEBUG
 
 	# Super noisy so we'll disable it.
-	for dataset in $(parsec libraries show_library $lib_id --contents | jq '.[] | select(.type == "file") | .id' -r); do
+	for dataset in $(curl --silent "${server}/api/libraries/${lib_id}/contents" | jq '.[] | select(.type == "file") | .id' -r); do
 		echo -n "$dataset "
 		for _ in $(seq 1 10); do
 			echo -n '.'
-			output="$(curl --silent "$server/api/libraries/datasets/$dataset/permissions?action=set_permissions&key=$GALAXY_API_KEY" --data '')"
+			output="$(curl --silent "${server}/api/libraries/datasets/${dataset}/permissions?action=set_permissions&key=$key" --data '')"
 			echo "$output" | grep --quiet access_dataset_roles
 			ec=$?
 			if (( ec == 0 )); then
@@ -24,7 +24,6 @@ setup_library() {
 		done;
 	done;
 }
-
 
 export IFS=$'\n';
 for line in $(cat secrets.json| jq '.[] | [.email, .key, .url, .lib] | @tsv' -r); do
