2276e110f7722d3bcf66d4594acf47ea5e2b4efc
Helena Rasche <hxr@hx42.org>
2019-07-09 13:28:20 +0200

    multiplex

--- run.sh
+++ run.sh
@@ -1,23 +1,37 @@
 #!/bin/bash
-set -x
 
-setup-data-libraries -g https://usegalaxy.eu -a $GALAXY_API_KEY -vvv --legacy -i GTN.yaml 2>&1 | grep --line-buffered -v DEBUG
+setup_library() {
+	server=$1
+	key=$2
+	lib_id=$3
 
-# Super noisy so we'll disable it.
-set +x
+	setup-data-libraries -g $server -a $key -vvv --legacy -i GTN.yaml 2>&1 | grep --line-buffered -v DEBUG
 
-for dataset in $(parsec libraries show_library 7d84c2ac21245dad --contents | jq '.[] | select(.type == "file") | .id' -r); do
-	echo -n "$dataset "
-	for _ in $(seq 1 10); do
-		echo -n '.'
-		output="$(curl --silent "https://usegalaxy.eu/api/libraries/datasets/$dataset/permissions?action=set_permissions&key=$GALAXY_API_KEY" --data '')"
-		echo "$output" | grep --quiet access_dataset_roles
-		ec=$?
-		if (( ec == 0 )); then
-			echo ''
-			break
-		else
-			sleep 1
-		fi
+	# Super noisy so we'll disable it.
+	for dataset in $(parsec libraries show_library $lib_id --contents | jq '.[] | select(.type == "file") | .id' -r); do
+		echo -n "$dataset "
+		for _ in $(seq 1 10); do
+			echo -n '.'
+			output="$(curl --silent "$server/api/libraries/datasets/$dataset/permissions?action=set_permissions&key=$GALAXY_API_KEY" --data '')"
+			echo "$output" | grep --quiet access_dataset_roles
+			ec=$?
+			if (( ec == 0 )); then
+				echo ''
+				break
+			else
+				sleep 1
+			fi
+		done;
 	done;
-done;
+}
+
+
+export IFS=$'\n';
+for line in $(cat secrets.json| jq '.[] | [.email, .key, .url, .lib] | @tsv' -r); do
+	email=$(echo "$line" | awk '{print $1}');
+	key=$(echo "$line" | awk '{print $2}');
+	url=$(echo "$line" | awk '{print $3}');
+	lib=$(echo "$line" | awk '{print $4}');
+	echo "Processing $url"
+	setup_library $url $key $lib
+done
