894b78a9de52a7b0968f3b85a512236cef7b7fa9
Gianmauro Cuccuru <4559911+gmauro@users.noreply.github.com>
2021-07-26 12:02:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-wget https://github.com/galaxyproject/training-material/archive/master.zip
-unzip master.zip
+wget https://github.com/galaxyproject/training-material/archive/main.zip
+unzip main.zip
 cd training-material-master/
 ./bin/mergeyaml.py --nondocker > ../GTN.yaml
 cd ../
